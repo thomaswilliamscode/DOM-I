d15ab77193912a04e4833affbb40404d99165963
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2023-09-06 12:30:29 -0400

    Almost Finished, Just need to look over README.md and finish up.

--- src/index.js
+++ src/index.js
@@ -58,9 +58,144 @@ console.log('project wired!')
 
 */
 let header = document.querySelector('header')
-let logo = document.querySelector('logo-img');
+let nav = header.querySelector('nav')
+let navLinks = nav.querySelectorAll('a')
+let logo = document.querySelector('#logo-img');
+
 let cta = document.querySelector('.cta');
 let ctaText = cta.querySelector('.cta-text');
-let mainH1 = ctaText.querySelector('h1');
-let button = ctaText.querySelector('button');
-let img1 = document.querySelector('cta-img');
+let ctaH1 = ctaText.querySelector('h1');
+let ctaButton = ctaText.querySelector('button');
+let img1 = document.querySelector('#cta-img');
+
+let mainContent = document.querySelector('.main-content');
+let topContent = mainContent.querySelector('.top-content');
+let middleImg = document.querySelector('#middle-img');
+let bottomContent = mainContent.querySelector('.bottom-content');
+
+let contactInfo = document.querySelector('.contact');
+
+// saving nav Link Text
+let idealNavLinks = ['Services', 'Product', 'Vision', 'Features', 'About','Contact']
+
+// storing the img filepaths in variables
+let newLogo = 'http://localhost:9000/img/logo.png'
+let img1Logo = "http://localhost:9000/img/cta.png"
+let middleLogo = "http://localhost:9000/img/accent.png"
+
+// Setting up Nav to use nav Link text
+for (let i = 0; i < navLinks.length; i++) {
+  let currentNav = navLinks[i]
+  let currentIdeal = idealNavLinks[i]
+  currentNav.textContent = currentIdeal;
+}
+
+// setting logo
+logo.src = newLogo;
+
+// setting up cta h1 and cta Button
+ctaH1.textContent = 'Dom Is Awesome'
+ctaButton.textContent = 'Get Started'
+
+// setting up img1 
+img1.src = img1Logo
+
+// storing information for mainContent text 
+let mainContentObject = {
+	'features-h4': 'Features',
+	'features-content':
+		'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+	'about-h4': 'About',
+	'about-content':
+		'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+	'services-h4': 'Services',
+	'services-content':
+		'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+	'product-h4': 'Product',
+	'product-content':
+		'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+	'vision-h4': 'Vision',
+	'vision-content':
+		'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+};
+
+// setting up variables to easily refrence the main content 
+let topContentArray = [
+	[ mainContentObject['features-h4'], mainContentObject['features-content'] ],
+  [ mainContentObject['about-h4'], mainContentObject['about-content'] ],
+];
+
+// getting the text-content classes inside of top content 
+let topContentDiv = topContent.querySelectorAll('.text-content')
+
+// setting top content 
+for (let i = 0; i < topContentDiv.length; i++) {
+  let currentDiv = topContentDiv[i]
+  let currentH4 = topContentDiv[i].querySelector('h4')
+  let currentP = topContentDiv[i].querySelector('p');
+
+  for (let j = 0; j < topContentArray[i].length; j++) {
+    let h4Content = topContentArray[i][0]
+    let pContent = topContentArray[i][1];
+
+    currentH4.textContent = h4Content
+    currentP.textContent = pContent
+  }
+
+}
+
+// linking middle img
+middleImg.src = middleLogo
+
+
+// storing Bottom Content 
+let bottomContentArray = [
+	[ mainContentObject['services-h4'], mainContentObject['services-content'] ],
+	[ mainContentObject['product-h4'], mainContentObject['product-content'] ],
+	[ mainContentObject['vision-h4'], mainContentObject['vision-content'] ],
+];
+
+// get Bottom content divs
+let bottomContentDiv = bottomContent.querySelectorAll('.text-content');
+
+// setting bottom content 
+for (let i = 0; i < bottomContentDiv.length; i++) {
+	let currentDiv = bottomContentDiv[i];
+	let currentH4 = bottomContentDiv[i].querySelector('h4');
+	let currentP = bottomContentDiv[i].querySelector('p');
+
+	for (let j = 0; j < bottomContentArray[i].length; j++) {
+		let h4Content = bottomContentArray[i][0];
+		let pContent = bottomContentArray[i][1];
+
+		currentH4.textContent = h4Content;
+		currentP.textContent = pContent;
+	}
+}
+
+
+
+// getting contact info info 
+let contactH4 = contactInfo.querySelector('h4')
+let contactPs = contactInfo.querySelectorAll('p');
+
+
+let contact = siteContent['contact']
+let contactH4Info = contact['contact-h4'];
+let contactAddress = contact['address']
+let contactPhone = contact['phone']
+let contactEmail = contact['email']
+
+// storing contact info into an array
+let contactArray = [contactAddress, contactPhone, contactEmail];
+
+// setting the contact h4 
+contactH4.textContent = contactH4Info
+
+// setting the rest of the contact section 
+for ( let i = 0; i < contactPs.length; i++ ) {
+  let currentP = contactPs[ i ];
+  let currentArray = contactArray[i]
+  currentP.textContent = currentArray
+}
+
